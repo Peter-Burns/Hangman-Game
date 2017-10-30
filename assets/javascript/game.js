5a8a0d661a1ca669293a2d6d5b0a5459390eac34
Peter Burns <peterburnshicks@gmail.com>
2017-10-30 04:15:12 -0400

    added hangman animation and game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,14 @@
-var guessLeft = 6
-var hangmanPic='';
-var wordLength='';
-var guessedLetters=[''];
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var guessLeft;
+var hangmanPic;
+var wordLength;
+var testWord="eee www";
+var guessedLetters;
+var wordSplit;
 var dictionary = ["soccer","football","goal","field","pitch","cleats","striker","defender","midfielder","goalkeeper","goalie","hat trick"];
-var currentWord= dictionary[Math.floor(dictionary.length*Math.random())];
-currentWord = "THERE WAS";
+var currentWord;
+var isGuessRight;
+var wins=0;
 function updateHangman (){
 	hangmanPic= "______   <br>" + "|&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp | <br>";
 	if(guessLeft === 6){
@@ -32,10 +36,15 @@ function updateHangman (){
 	document.getElementById("hangmanAscii").innerHTML = hangmanPic;
 }
 function drawCurrentWord() {
+	wordLength=[''];
 	for (var i = 0; i < currentWord.length; i++) {
+
 		if(currentWord.charAt(i) === ' ')
 		{
 			wordLength+=' &nbsp&nbsp'
+		}
+		else if(guessedLetters.indexOf(currentWord.charAt(i))!== -1){
+			wordLength+= currentWord.charAt(i) + '&nbsp';
 		}
 		else{
 			wordLength+='_&nbsp';
@@ -49,8 +58,60 @@ function updateGuessesLeft(){
 function updateLettersGuessed(){
 	document.getElementById("guessedLetters").innerHTML = guessedLetters;
 }
-updateGuessesLeft();
-updateHangman();
-drawCurrentWord();
-updateLettersGuessed();
+function updateWins(){
+	document.getElementById("wins").innerHTML = "Wins: " + wins;
+}
+function resetGame() {
+	guessLeft = 6;
+	guessedLetters=[];
+	currentWord= dictionary[Math.floor(dictionary.length*Math.random())];
+	updateGuessesLeft();
+	updateHangman();
+	drawCurrentWord();
+	updateLettersGuessed();
+	updateWins();
+}
+function checkGuess(guess){
+	isGuessRight = false;
+	wordSplit = currentWord.split('');
+	for(x in wordSplit){
+		if(guess===wordSplit[x]){
+			console.log(x);
+			isGuessRight = true;
+		}
+	}
+	console.log(isGuessRight);
+	return isGuessRight;
+}
 
+resetGame();
+document.onkeyup = function (event){
+	if(alphabet.indexOf(event.key)>-1 && guessedLetters.indexOf(event.key) == -1)
+	{
+		guessedLetters.push(event.key);
+		guessLeft--;
+		checkGuess(event.key);
+		if(isGuessRight){
+			guessLeft++;
+			drawCurrentWord();
+		}
+		if(wordLength.replace(/&nbsp/g, '')===currentWord)
+		{
+			alert("You win!");
+			wins++;
+			resetGame();
+			return
+		}
+		if(guessLeft===0){
+			alert("You lose!");
+			resetGame();
+			return;
+		}
+		updateGuessesLeft();
+		updateHangman();
+		updateLettersGuessed();
+	}
+	else{
+		return;
+	}
+};
